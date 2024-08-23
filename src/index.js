e8051bceaded8e1d364b704f1eabc896a7bd5b16
Aaron <yangtao.yangtao@antgroup.com>
2024-08-23 12:56:21 +0800

    refactor: rewrite with cjs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import core from '@actions/core';
-import { Octokit } from '@octokit/rest';
-import { translate } from './translate';
+const core = require('@actions/core');
+const { Octokit } = require('@octokit/rest');
+const axios = require('axios');
 
 const token = process.env.GITHUB_TOKEN;
+const key = process.env.AZURE_TRANSLATE_KEY;
+const endpoint = process.env.AZURE_TRANSLATE_ENDPOINT;
+const location = process.env.AZURE_TRANSLATE_LOCATION;
+const to = process.env.AZURE_TRANSLATE_TARGET || 'en';
 const octokit = new Octokit({ auth: token });
 
 async function run() {
@@ -38,6 +42,29 @@ async function run() {
     );
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
+  }
+}
+
+async function translate(text) {
+  try {
+    const response = await axios.post(
+      `${endpoint}/translate?api-version=3.0&from=zh-Hans&to=${to}`,
+      [{ text }],
+      {
+        headers: {
+          'Ocp-Apim-Subscription-Key': key,
+          'Ocp-Apim-Subscription-Region': location,
+          'Content-type': 'application/json',
+          'X-ClientTraceId': crypto.randomUUID(),
+        },
+      }
+    );
+
+    const translatedText = response.data[0].translations[0].text;
+    return translatedText;
+  } catch (error) {
+    console.error('Error translating text:', error);
+    throw error;
   }
 }
 
--- test/translate.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { translate } from '../src/translate.js';
-
-translate('你好').then((result) => {
-  console.log(result);
-});
